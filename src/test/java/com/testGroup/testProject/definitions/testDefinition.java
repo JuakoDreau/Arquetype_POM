a164f7762b7a26fc869954f97fa308aa38043737
juakodreau <joaquindreau@gmail.com>
2024-04-08 15:04:05 -0400

    Flujo facil Hola Mundo

--- src/test/java/com/testGroup/testProject/definitions/testDefinition.java
+++ src/test/java/com/testGroup/testProject/definitions/testDefinition.java
@@ -13,27 +13,22 @@ public class testDefinition {
 
     @Given("abro el navegador")
     public void abro_el_navegador() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hola Mundo 01");
     }
     @When("navego en google {string}")
     public void navego_en_google(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hola Mundo 02");
     }
     @And("ingreso los datos para sumar {string}")
     public void ingreso_los_datos_para_sumar(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hola Mundo 03");
     }
     @And("busco el resultado")
     public void busco_el_resultado() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hola Mundo 04");
     }
     @Then("valido que el resultado sea correcto\"{int}\"")
     public void valido_que_el_resultado_sea_correcto(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("Hola Mundo 05");
     }
 }
--- src/test/java/com/testGroup/testProject/myHooks/Hooks.java
+++ src/test/java/com/testGroup/testProject/myHooks/Hooks.java
@@ -2,9 +2,9 @@ package com.testGroup.testProject.myHooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -12,19 +12,13 @@ public class Hooks {
     public static WebDriver driver;
 
     @Before
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    }
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
 
+    }
     @After
-    public void tearDown() throws Exception {
+    void teardown() {
         driver.quit();
     }
 }
